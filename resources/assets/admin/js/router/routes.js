ed60baf01e5834483f358528fc8d4e13051165ec
neronmoon <alistar.neron@gmail.com>
2016-11-08 09:25:04 +0200

    foobar

--- resources/assets/admin/js/router/routes.js
+++ resources/assets/admin/js/router/routes.js
@@ -1,31 +1,31 @@
 // don't import anything, make sure it's just config
 const routes = [
-  {
-    path: '/',
-    component: resolve =>resolve(require('../views/Home/index.vue'))
-  },
-  {
-    path: '/dashboard',
-    name: 'dashboard',
-    auth: true,
-    meta: {
-      requiresAuth: true
+    {
+        path: '/',
+        component: resolve => resolve(require('../views/Home/index.vue'))
     },
-    component: resolve =>resolve(require('../views/Account/Dashboard.vue'))
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: resolve =>resolve(require('../views/Account/Login.vue'))
-  },
-  {
-    path: '/logout',
-    component: resolve =>resolve(require('../views/Account/Logout.vue'))
-  },
-  {
-    path: '*',
-    redirect: '/'
-  }
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        auth: true,
+        meta: {
+            requiresAuth: true
+        },
+        component: resolve => resolve(require('../views/Account/Dashboard.vue'))
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: resolve => resolve(require('../views/Account/Login.vue'))
+    },
+    {
+        path: '/logout',
+        component: resolve => resolve(require('../views/Account/Logout.vue'))
+    },
+    {
+        path: '*',
+        redirect: '/'
+    }
 ]
 
 export default routes
